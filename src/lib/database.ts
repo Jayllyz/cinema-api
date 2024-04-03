382a9129a58114545e743d492a354d1e145ca77c
Antony David <antonydavid945@gmail.com>
2024-04-03 17:16:38 +0200

    fix: small shorthand syntax

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -11,7 +11,7 @@ export async function getOverlapingScreenings(
 ): Promise<Screenings | null> {
   return await prisma.screenings.findFirst({
     where: {
-      room_id: room_id,
+      room_id,
       OR: [
         {
           AND: [{start_time: {gte: start_time}}, {end_time: {lte: end_time}}],
@@ -34,7 +34,7 @@ export async function getOverlapingShift(
 ): Promise<Working_shifts | null> {
   return await prisma.working_shifts.findFirst({
     where: {
-      position: position,
+      position,
       OR: [
         {
           AND: [{start_time: {gte: start_time}}, {end_time: {lte: end_time}}],
