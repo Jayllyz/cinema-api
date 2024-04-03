2fab33d055620a48814b0c80ac9172da4a276cb4
Antony David <antonydavid945@gmail.com>
2024-04-03 21:09:13 +0200

    fix: await

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use((c, next) => {
   return next();
 });
 
-app.use(async (c, next) => {
+app.use((c, next) => {
   if (c.req.method === 'POST' || c.req.method === 'PUT' || c.req.method === 'PATCH') {
     const contentType = c.req.header('content-type');
     const url = c.req.url;
