460eb4d22995a728cc338662d785f5548c0c7a9e
Antony David <antonydavid945@gmail.com>
2024-05-03 16:59:35 +0200

    fix: data convert insertion

--- src/validators/movies.ts
+++ src/validators/movies.ts
@@ -16,7 +16,7 @@ export const insertMovieValidator = z.object({
   title: z.string(),
   description: z.string(),
   author: z.string(),
-  release_date: z.string().date(),
+  release_date: z.coerce.date(),
   duration: z.number().positive(),
   status: z.string(),
   category_id: z.number().positive(),
@@ -25,7 +25,7 @@ export const insertMovieValidator = z.object({
 export const updateMovieValidator = z.object({
   title: z.string().optional(),
   author: z.string().optional(),
-  release_date: z.string().date().optional(),
+  release_date: z.coerce.date().optional(),
   description: z.string().optional(),
   duration: z.number().positive().optional(),
   status: z.string().optional(),
